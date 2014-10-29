2984a113bf1356b4445eb0cff6097ead03ee218f
Alexander Amanuel <alexander.amanuel@skype.net>
2014-10-29 17:16:31 +0300

    Fedora compatibility

--- fogger_lib/Bridge.py
+++ fogger_lib/Bridge.py
@@ -12,42 +12,20 @@
 # You should have received a copy of the GNU General Public License along
 # with this program.  If not, see <http://www.gnu.org/licenses/>.
 ### END LICENSE
-from gi.repository import Gtk, Unity, Notify, Dbusmenu, TelepathyGLib
+from gi.repository import Gtk, Notify
 
 Notify.init('fogger')
-
-
-TELEPATHY_PRESENCE_MAP = {
-    TelepathyGLib.ConnectionPresenceType.AVAILABLE: 'available',
-    TelepathyGLib.ConnectionPresenceType.AWAY: 'away',
-    TelepathyGLib.ConnectionPresenceType.EXTENDED_AWAY: 'away',
-    TelepathyGLib.ConnectionPresenceType.BUSY: 'busy',
-    TelepathyGLib.ConnectionPresenceType.OFFLINE: 'offline',
-}
-
 
 class DesktopBridge:
     icon_name = None
     desktop_file = None
     actions = {}
-    launcher_actions = {}
 
     def __init__(self, root, desktop_file, icon_name=None):
         self.W = root
         self.desktop_file = desktop_file
         self.icon_name = icon_name
-        self.launcher_entry = Unity.LauncherEntry.get_for_desktop_file(self.desktop_file)
-        self.quicklist = Dbusmenu.Menuitem.new()
-        self.launcher_entry.set_property("quicklist", self.quicklist)
         self.indicator = None
-        self._rename_methods = {
-            Dbusmenu.Menuitem: self._rename_dbus_menu_item,
-            Gtk.MenuItem: self._rename_gtk_menu_item,
-        }
-        self.telepathy_account_manager = TelepathyGLib.AccountManager.new(
-                                             TelepathyGLib.DBusDaemon.dup())
-        self.telepathy_account_manager.connect(
-            'most-available-presence-changed', self.notify_presence_change)
         self.W.connect('notify::is-active', self.notify_window_state)
 
     def _js(self, W, jscode):
@@ -66,77 +44,48 @@ class DesktopBridge:
         js = js + 'e.foggerData = params; document.dispatchEvent(e);'
         self._js(W, js)
 
-    def _rename_gtk_menu_item(self, item, name):
-        item.props.label = name
-
-    def _rename_dbus_menu_item(self, item, name):
-        item.property_set(Dbusmenu.MENUITEM_PROP_LABEL, name)
-
     def rename_item(self, W, data):
-        widget_id = data['id'][0]
-        item = self.widgets.get(widget_id)
-        rename = self._rename_methods.get(item.__class__)
-        if rename:
-            rename(item, data['name'][0])
+        pass
 
     def notify_window_state(self, window, active):
         self._dispatch_dom_event(self.W, 'foggerWindowStateChange', {
                 'active': self.W.props.is_active})
 
     def notify_presence_change(self, manager, presence, status, message):
-        self._dispatch_dom_event(None, 'foggerPresenceChange',
-                {'presence': TELEPATHY_PRESENCE_MAP.get(presence, 'offline')})
+        pass
 
     def get_presence(self, W, data):
-        presence = self.telepathy_account_manager.get_most_available_presence()
-        print W
+        pass
 
     def notify(self, W, data):
         Notify.Notification.new(data.get('summary', [''])[0], data.get('body', [''])[0], self.icon_name).show()
 
     def set_progress(self, W, data):
-        self.launcher_entry.props.progress = float(data['progress'][0])
-        self.launcher_entry.props.progress_visible = True
+        pass
 
     def clear_progress(self, W, data):
-        self.launcher_entry.props.progress_visible = False
+        pass
 
     def set_count(self, W, data):
-        self.launcher_entry.props.count = int(data['count'][0])
-        self.launcher_entry.props.count_visible = True
+        pass
 
     def clear_count(self, W, data):
-        self.launcher_entry.props.count_visible = False
+        pass
 
     def set_urgent(self, W, data):
-        self.launcher_entry.props.urgent = True
+        pass
 
     def clear_urgent(self, W, data):
-        self.launcher_entry.props.urgent = False
+        pass
 
     def add_launcher_action(self, W, data):
-        name = data['name'][0]
-        if name in self.launcher_actions:
-            return
-        item = self.launcher_actions[name] = Dbusmenu.Menuitem.new()
-        item.property_set(Dbusmenu.MENUITEM_PROP_LABEL, name)
-        item.property_set_bool(Dbusmenu.MENUITEM_PROP_VISIBLE, True)
-        item.connect('item-activated', lambda *a, **kw:
-                self._dispatch_dom_event(None, 'foggerQLCallbackEvent',
-                    {'name': name}))
-        self.quicklist.child_append(item)
+        pass
 
     def remove_launcher_action(self, W, data):
-        name = data['name'][0]
-        action = self.launcher_actions.get(name)
-        if action:
-            self.quicklist.child_delete(action)
-            del self.launcher_actions[name]
+        pass
 
     def remove_launcher_actions(self, W, data):
-        for action in self.launcher_actions.values():
-            self.quicklist.child_delete(action)
-        self.launcher_actions = {}
+        pass
 
     def add_action(self, W, data):
         action_path = data['name'][0]
